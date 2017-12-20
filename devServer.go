d78642eb629210c6775de66867ed6fdb54d46b48
Christian Dreier <dreier.christian@googlemail.com>
2017-12-21 00:04:24 +0100

    added debounce function

--- devServer.go
+++ devServer.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
+	"time"
 
 	"github.com/fsnotify/fsnotify"
 	"github.com/gorilla/websocket"
@@ -32,6 +34,21 @@ func (h *holder) ws(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
+func debounce(interval time.Duration, input chan string, f func(arg string)) {
+	var item string
+	timer := time.NewTimer(interval)
+	for {
+		select {
+		case item = <-input:
+			timer.Reset(interval)
+		case <-timer.C:
+			if item != "" {
+				f(item)
+			}
+		}
+	}
+}
+
 func startFileWatcher(dir string) {
 	watcher, err := fsnotify.NewWatcher()
 	if err != nil {
@@ -39,15 +56,17 @@ func startFileWatcher(dir string) {
 	}
 	defer watcher.Close()
 
+	eventChan := make(chan string)
+	go debounce(time.Second, eventChan, func(name string) {
+		fmt.Println("RELOAD! ", name)
+	})
+
 	done := make(chan bool)
 	go func() {
 		for {
 			select {
 			case event := <-watcher.Events:
-				log.Println("event:", event)
-				if event.Op&fsnotify.Write == fsnotify.Write {
-					log.Println("modified file:", event.Name)
-				}
+				eventChan <- event.Name
 			case err := <-watcher.Errors:
 				log.Println("error:", err)
 			}
